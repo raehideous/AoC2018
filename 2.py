d756a300f9da770e9b3fd568d9e10ab7601b93dd
patryk.wegner <patryk.wegner@venturedevs.com>
2018-12-02 18:15:53 +0100

    Puzzle 2

--- 2.py
+++ 2.py
@@ -1,12 +1,31 @@
 from aocd import get_data
+from collections import Counter
 
 def part_one(input):
-    res=''
-    print("Part One: ", res)
+    counts=[0, 0] #array of occurences two and three times
+
+    for word in input.split():
+        occurences_count=[j for i, j in Counter(word).most_common()]
+        if 3 in occurences_count:
+            counts[0]+=1
+        if 2 in occurences_count:
+            counts[1]+=1
+
+    print('Part One: ', counts[0]*counts[1])
+
 
 def part_two(input):
-    res=''
-    print("Part Two: ", res)
+    words=input.split()
+    for word in words:
+        for wordCmp in words:
+            diffs = 0
+            for idx, char in enumerate(word):
+                if char != wordCmp[idx]:
+                    diffs+=1
+            if diffs==1:
+                difference=[ch for idx, ch in enumerate(word) if ch == wordCmp[idx]]
+                print('Part Two: ', ''.join(difference))
+                return
 
 
 data = get_data(day=2)
