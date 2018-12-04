7b9b8a9bc6d9a68e58c3bd8a7a2c3a9cf485b0ee
patryk.wegner <patryk.wegner@venturedevs.com>
2018-12-04 19:51:57 +0100

    3rd day

--- 3.py
+++ 3.py
@@ -1,14 +1,25 @@
 from aocd import get_data
+import re
+import numpy as np
 
-def part_one(input):
-    res=''
-    print("Part One: ", res)
+def part_one(claims):
+    fabric=np.zeros((1000, 1000))
+    for claim, start_x, start_y, width, height in claims:
+        fabric[start_x:start_x + width, start_y:start_y + height] += 1
 
-def part_two(input):
-    res=''
-    print("Part Two: ", res)
+    print(np.sum(fabric > 1))
+    return fabric
+
+
+
+def part_two(claims, fabric):
+    for claim, start_x, start_y, width, height in claims:
+        if np.all(fabric[start_x:start_x + width, start_y:start_y + height]==1):
+            print("Part Two: ", claim)
+            return claim
 
 
 data = get_data(day=3)
-part_one(data)
-part_two(data)
+claims=[[*map(int, re.findall(r'\d+', l))] for l in data.splitlines() if l]
+fabric=part_one(claims)
+part_two(claims, fabric)
