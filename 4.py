9f7fd468dc447bfe18dcf7e241e6538f56e8b6b4
patryk.wegner <patryk.wegner@venturedevs.com>
2018-12-05 23:07:11 +0100

    Day 5 solved

--- 4.py
+++ 4.py
@@ -1,14 +1,52 @@
+from collections import defaultdict
 from aocd import get_data
+from datetime import datetime
+
+def get_minutes(time):
+    return int(time[-2:])
+
 
 def part_one(input):
-    res=''
-    print("Part One: ", res)
+    guards_times = defaultdict(int)
+    times = defaultdict(int)
+    guard = None
+    asleep = None
+
+    for line in input:
+        time, action = line.split('] ')
+        time = get_minutes(time)
+        if 'Guard' in action:
+            guard = int(action.split()[1][1:])
+            asleep = None
+        elif 'falls asleep' in action:
+            asleep = time
+        elif 'wakes up' in action:
+            # guardsTimes[guard] =
+            times[guard] += (time - asleep)
+            for t in range(asleep, time):
+                guards_times[(guard, t)] += 1
+
+    top_time = 0
+    top_guard = 0
+    for key in times:
+        if times[key] > top_time:
+            top_time = times[key]
+            top_guard = key
+
+    top = None
+    for key, val in guards_times.items():
+        guard, time = key
+        if top is None or val > guards_times[top]Z:
+            top = key
+
+    print(top_guard, top_time, top, top[0] * top[1])
 
 def part_two(input):
     res=''
     print("Part Two: ", res)
 
 
-data = get_data(day=4)
+data=get_data(day=4).splitlines()
+data.sort()
 part_one(data)
 part_two(data)
--- 5.py
+++ 5.py
@@ -1,14 +1,30 @@
 from aocd import get_data
 
-def part_one(input):
-    res=''
-    print("Part One: ", res)
 
-def part_two(input):
-    res=''
-    print("Part Two: ", res)
+def are_equal(a, b):
+    res=(a.lower() == b.lower() and
+            ((a.isupper() and b.islower()) or
+             (a.islower() and b.isupper())))
+    return res
+
+
+def part_one(input):
+    buffer = []
+    for c in input:
+        if buffer and are_equal(c, buffer[-1]):
+            buffer.pop()
+        else:
+            buffer.append(c)
+
+    return len(buffer)
+
+
+def part_two(polymer):
+    letters = set([c.lower() for c in polymer])
+    minimal_count = min([part_one(polymer.replace(l, '').replace(l.upper(), '')) for l in letters])
+    return minimal_count
 
 
 data = get_data(day=5)
-part_one(data)
-part_two(data)
+print('Part One: ', part_one(data))
+print('Part Two: ', part_two(data))
